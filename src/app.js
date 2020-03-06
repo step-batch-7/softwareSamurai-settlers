149745d433b4ea54b64214484b5f7b7ad60c1578
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-06 18:06:00 +0530

    |#7|Anuja/Sruthy| Added handler to get terrain details
    added handler that takes request for terrains and returns the details of
    each terrain
    
    What changes have you made?
    *
    
    Why do we need this change?
    *
    
    What can this change impact?
    *

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const { getTerrainDetails } = require('./handlers');
+
 const app = express();
 app.use(express.static('public'));
 app.get('/', (req, res) => res.redirect('catan.html'));
+app.get('/terrains', getTerrainDetails);
 module.exports = { app };
