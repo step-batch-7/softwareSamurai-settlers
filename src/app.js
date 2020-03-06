15cbbdc3f39d2f085e4a7f7ebe87078a606175c2
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-06 16:47:55 +0530

    |#7|Anuja/Sruthy| Added a handler for /
    Added a handler that redirects to catan.html after hitting / url
    
    In order to avoid the error cannot get /

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const app = express();
 app.use(express.static('public'));
-
+app.get('/', (req, res) => res.redirect('catan.html'));
 module.exports = { app };
