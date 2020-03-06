c1b648c2d3e28105a172ac652522311f004224eb
Rahit <rahitkar@users.noreply.github.com>
2020-03-06 18:43:55 +0530

    |#9|rahit| nagendra|-added handler for bank status
    What changes have you made?
    *
    added handler for bank status
    created Bank class
    renamed ids of crop  and devCard to grain and developmentCard of catan page
    
    Why do we need this change?
    *
    to show the bank status to the player
    What can this change impact?
    *
    player can see the bank status

--- src/handlers.js
+++ src/handlers.js
@@ -1,10 +1,15 @@
 const { Board } = require('./models/board');
+const Bank = require('./models/bank');
 
 const getTerrainDetails = function(req, res) {
   const board = new Board();
   res.json(board.getTerrains());
 };
 
-module.exports = {
-  getTerrainDetails
+const getBankStatus = (req, res) => {
+  const bank = new Bank();
+  const bankStatus = bank.getBankStatus();
+  res.end(JSON.stringify(bankStatus));
 };
+
+module.exports = { getBankStatus, getTerrainDetails };
