bf984b055bd0704f85f3884fd987cf4694b9228e
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-07 10:27:14 +0530

    |#7|Anuja/Sruthy| Added integration test
    Added tests for static files and getTerrainDetails handlers

--- test/integration/testHandlers.js
+++ test/integration/testHandlers.js
@@ -1,5 +1,29 @@
 const request = require('supertest');
-const {app} = require('../../src/app');
+const { app } = require('../../src/app');
+
+describe('Get static file', () => {
+  it('Should redirected to catan page for /', done => {
+    request(app)
+      .get('/')
+      .expect(302, done)
+      .expect('Location', 'catan.html');
+  });
+
+  it('Should give catan page for catan.html ', done => {
+    request(app)
+      .get('/catan.html')
+      .expect(200, done);
+  });
+});
+
+describe('Get getTerrainDetails', () => {
+  it('Should give terrain details for /terrains', done => {
+    request(app)
+      .get('/terrains')
+      .expect(200, done)
+      .expect(/"i":{"noToken":9,"resource":"forest"}/);
+  });
+});
 
 describe('Get /cardsCount', () => {
   it('should give count of resource and devCards', done => {
