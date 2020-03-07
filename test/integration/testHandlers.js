edeef89b9da1871ffde45fa8cd0afd91463f8fcc
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-03-07 10:04:40 +0530

    |#8|revathi/saurabh| - integration test for cardsCount
    added test to check content type and response status code

---
+++ test/integration/testHandlers.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+const {app} = require('../../src/app');
+
+describe('Get /cardsCount', () => {
+  it('should give count of resource and devCards', done => {
+    request(app)
+      .get('/cardsCount')
+      .expect(200, done)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+  });
+});
