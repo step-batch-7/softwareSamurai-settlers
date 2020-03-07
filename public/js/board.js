d72da2ba20268ca7112b22b94685489a573762e6
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-03-07 10:13:12 +0530

    |#7|Anuja/Sruthy| Aligned the number token on each terrain
    and removed console log in the board.js

--- public/js/board.js
+++ public/js/board.js
@@ -3,7 +3,6 @@ const getTerrains = async function() {
   if (response.ok) {
     const terrainsInfo = await response.json();
     const terrains = document.getElementsByClassName('terrain');
-    console.log(terrainsInfo, terrains);
     Array.from(terrains).forEach(terrain => {
       if (terrainsInfo[terrain.id].resource === 'desert') {
         const html = `<image class="terrain-image"
@@ -17,7 +16,7 @@ const getTerrains = async function() {
       const html = `<image class="terrain-image" xlink:href='/assets/terrains/${
         terrainsInfo[terrain.id].resource
       }.jpg' count="${terrainsInfo[terrain.id].noToken}"></image>
-      <circle cx="50" cy="50" r="17" fill="burlywood" opacity="0.7"/>
+      <circle cx="50" cy="55" r="17" fill="burlywood" opacity="0.7"/>
       <text x="50%" y="50%"  class="number-token" >
       ${terrainsInfo[terrain.id].noToken}</text>`;
       terrain.innerHTML += html;
