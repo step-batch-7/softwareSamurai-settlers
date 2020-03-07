07e88ebf38a354a0bdf2f44289f277b127e42c20
Rahit <rahitkar@users.noreply.github.com>
2020-03-07 11:09:06 +0530

    |#9|rahit/nagendra| - changed xmlHttpRequest to fetch

--- public/js/bank.js
+++ public/js/bank.js
@@ -1,22 +1,12 @@
-const getBankStatus = function() {
-  requestHttp('GET', '/bankStatus', null, cards => {
+const getBankStatus = async function() {
+  const response = await fetch('/bankStatus');
+  if (response.ok) {
+    const cards = await response.json();
     const bankElement = document.getElementById('bank');
-
     const cardNames = Object.keys(cards);
     cardNames.forEach(cardName => {
       const card = bankElement.querySelector(`#${cardName}`);
       card.querySelector('text').innerHTML = cards[cardName];
     });
-  });
-};
-
-const requestHttp = (method, url, data, callBack) => {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    if (this.status === 200) {
-      callBack(JSON.parse(this.responseText));
-    }
-  };
-  req.open(method, url, true);
-  req.send(data);
+  }
 };
