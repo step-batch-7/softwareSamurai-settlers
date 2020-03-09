1250fd71001f3a044ebc76781e46485af4fe4448
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-03-09 15:18:18 +0530

    |#23|revathi/saurabh| - added test for haveResource for bank

--- test/unit/testBank.js
+++ test/unit/testBank.js
@@ -35,4 +35,14 @@ describe('Bank', () => {
       assert.isFalse(bank.remove({ count: 1 }));
     });
   });
+  describe('haveResource', () => {
+    it('should give true when bank have enough resources', () => {
+      const bank = new Bank();
+      assert.isTrue(bank.haveResource('ore', 1));
+    });
+    it('should give false when bank have not enough resources', () => {
+      const bank = new Bank();
+      assert.isFalse(bank.haveResource('ore', 20));
+    });
+  });
 });
